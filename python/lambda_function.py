39a6c199be64e55ef21b3639e773284482e296bd
Yoimer Román <ydrf19@gmail.com>
2020-08-15 15:23:35 -0400

    saves device_id and hardcoded geo-location on dynamo

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -9,13 +9,20 @@ def lambda_handler(event, context):
     tableName = "LOCATION"
     dynamodb_client = boto3.client('dynamodb')
     table = dynamodb.Table(tableName)
+
+    # if id exists in db, it updates those values
+    # if id does not exists in db, it just adds it with all the values comming from event
+    # boards do not sent their geo-location. that's why these values are 'hardcoded' on db
     response = table.put_item(
         Item = {
            'device_id': event['id'],
-           'latitude': event['latitude']
+           'latitude': '28.785969',
+           'longitude': '-81.332263',
+           'address': '363 Silver Pine Dr',
+           'site': 'Lake Mary',
+           'site_id': '10'
         }
     )
-    
 
     print("PutItem succeeded:")
     print(json.dumps(response, indent = 4))
